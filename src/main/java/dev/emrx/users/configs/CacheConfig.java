e1847f3cee00e5f6f800da89af422cf70e73d9ac
Elvis M <u201112959@upc.edu.pe>
2024-06-06 20:51:51 -0500

    agregando redis para administrar el cache en el proyecto

--- src/main/java/dev/emrx/users/configs/CacheConfig.java
+++ src/main/java/dev/emrx/users/configs/CacheConfig.java
@@ -1,18 +1,37 @@
 package dev.emrx.users.configs;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.redisson.spring.cache.RedissonSpringCacheManager;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableCaching
 public class CacheConfig {
 
     @Bean
-    public CacheManager getCache() {
-        return new ConcurrentMapCacheManager("users");
+    public CacheManager getCache(RedissonClient redissonClient) {
+        Map<String, CacheConfig> config = new HashMap<>();
+        config.put("users", new CacheConfig());
+
+        return new RedissonSpringCacheManager(redissonClient);
+    }
+
+    @Bean(destroyMethod = "shutdown")
+    public RedissonClient redisson() {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://localhost:6379/0")
+                .setPassword("redis2024");
+
+        return Redisson.create(config);
     }
 
 }
--- src/main/java/dev/emrx/users/entities/User.java
+++ src/main/java/dev/emrx/users/entities/User.java
@@ -5,12 +5,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 8544435213923108026L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
